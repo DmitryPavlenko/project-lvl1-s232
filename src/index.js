c93445db6779cbe867762235f78e8d8c81d29010
DmitryPavlenko <azeos1101@ya.ru>
2018-03-06 14:13:28 +0300

    [2] Welcome prompt

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const getName = () => readlineSync.question('May I have your name?: ');
+
+const welcome = () => {
+  const name = getName();
+
+  console.log(`Hello, ${name}!`)
+}
+
+export default welcome;
