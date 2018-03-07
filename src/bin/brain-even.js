3f39563ca49c5f0598128949f69fd95c0a9cc0e2
Dmitry Pavlenko <azeos1101@ya.ru>
2018-03-07 22:51:01 +0300

    [4] style fixes

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
 
-import { getName, welcome, greeting, evenGame } from '..';
+import { welcome, evenGame } from '..';
 
 welcome();
 
 console.log('Answer "yes" if number even otherwise answer "no".');
-
-// greeting(userName);
 
 evenGame();
--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
 import readlineSync from 'readline-sync';
 
-export const getName = () => readlineSync.question('May I have your name?: ');
+const maxNum = 100;
+const maxTries = 3;
 
-export const welcome = () => console.log('Welcome to the Brain Games!');
+const isEven = (num) => {
+  if (num % 2 === 0) {
+    return 'yes';
+  }
 
-export const greeting = name => console.log(`Hello, ${name || getName()}!`);
+  return 'no';
+};
 
-export const isEven = (num) => num % 2 === 0 ? 'yes' : 'no';
+const getName = () => readlineSync.question('May I have your name?: ');
 
-export const evenGame = () => {
+const welcome = () => console.log('Welcome to the Brain Games!');
+
+const greeting = name => console.log(`Hello, ${name || getName()}!`);
+
+const evenGame = () => {
   const userName = getName();
-  let count = 0
-  do {
-    let num = Math.ceil(Math.random()*100);
+  let count = 0;
 
+  do {
+    const num = Math.ceil(Math.random() * maxNum) + 1;
     console.log(`Question: ${num}`);
-    let answer = readlineSync.question('Your answer: ');
-    
-    if(answer === isEven(num)) {
+
+    const answer = readlineSync.question('Your answer: ');
+
+    if (answer === isEven(num)) {
       console.log('Correct!');
       count += 1;
-    } else { 
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(num)}'. \nLet's try again, ${userName}!`);
+    } else {
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven(num)}'.\nLet's try again, ${userName}!`);
       return;
     }
-   } while (count < 3);
+  } while (count < maxTries);
 
   console.log(`Congratulations, ${userName}!`);
 };
+
+export { welcome, greeting, evenGame };
