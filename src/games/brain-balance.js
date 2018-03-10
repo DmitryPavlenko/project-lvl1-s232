84b8a9a3b037ebe2ef6308cd4f8caa86b451cf6b
Dmitry Pavlenko <azeos1101@ya.ru>
2018-03-10 11:52:31 +0300

    [7] added Balance game

---
+++ src/games/brain-balance.js
@@ -0,0 +1,27 @@
+import { cons } from 'hexlet-pairs';
+import { getRandom, makeGame } from '..';
+
+const balance = (num) => {
+  const digits = String(num).split('').sort((a, b) => a - b);
+  const length = digits.length - 1;
+  const first = Number(digits[0]);
+  const last = Number(digits[length]);
+  if (last - first <= 1) {
+    return digits.join('');
+  }
+
+  const result = (first + 1) + digits.join('').substr(1, length - 1) + (last - 1);
+  return balance(result);
+};
+
+const rules = 'Balance the given number.';
+
+const game = () => {
+  const maxNum = 10000;
+  const num = getRandom(maxNum, 1);
+  const quest = num;
+  const sol = balance(num);
+  return cons(quest, sol);
+};
+
+export default () => makeGame(game, rules);
