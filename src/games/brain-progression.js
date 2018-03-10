18bb009b25609ccac8ce770876b60d19d2a1da24
Dmitry Pavlenko <azeos1101@ya.ru>
2018-03-10 13:27:43 +0300

    [8] create game-progression logic

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,7 +1,28 @@
-import { makeGameFlow } from '..';
+import { cons } from 'hexlet-pairs';
+import { getRandom, makeGameFlow } from '..';
 
 const rules = '';
 
-const gameLogic = () => {};
+const gameLogic = () => {
+  const rowLength = 10;
+  const step = getRandom(10, 1);
+  const startNum = getRandom(10);
+  const skipPosition = getRandom(9, 1);
+  let skipped;
+  let row = String(startNum);
+  let acc = startNum;
+  for (let i = 1; i < rowLength; i += 1) {
+    acc += step;
+    if (i === skipPosition) {
+      skipped = acc;
+      row += ' ..';
+    } else {
+      row += ` ${acc}`;
+    }
+  }
+  const quest = row;
+  const sol = String(skipped);
+  return cons(quest, sol);
+};
 
 export default () => makeGameFlow(gameLogic, rules);
