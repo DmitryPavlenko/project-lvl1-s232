9a7b5f84d36dae52f15b696d73bc23df82b8256b
Dmitry Pavlenko <azeos1101@ya.ru>
2018-03-11 12:21:57 +0300

    [8] exclude loop in progression game

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,26 +3,37 @@ import { getRandom, makeGameFlow } from '..';
 
 const rules = 'What number is missing in this progression?';
 
-const gameLogic = () => {
-  const rowLength = 10;
+const getProgressionArray = (length = 10) => {
+  const start = getRandom(10);
   const step = getRandom(10, 1);
-  const startNum = getRandom(10);
-  const skipPosition = getRandom(9, 1);
-  let skipped;
-  let row = String(startNum);
-  let acc = startNum;
-  for (let i = 1; i < rowLength; i += 1) {
-    acc += step;
-    if (i === skipPosition) {
-      skipped = acc;
-      row += ' ..';
-    } else {
-      row += ` ${acc}`;
+
+  const iter = (counter, accu) => {
+    if (counter > length) {
+      return accu;
     }
-  }
-  const quest = row;
-  const sol = String(skipped);
+    const last = [(counter === 0) ? start : accu[counter - 1] + step];
+    return iter(counter + 1, accu.concat(last));
+  };
+  return iter(0, []);
+};
+
+const getDataWithSkipped = (array, skipNumber) => {
+  const iter = (counter, accu) => {
+    if (counter >= array.length) {
+      return accu;
+    }
+    const item = (counter === skipNumber ? '..' : array[counter]);
+    return iter(counter + 1, `${accu} ${item}`);
+  };
+  return iter(0, '');
+};
+
+const generateData = () => {
+  const array = getProgressionArray();
+  const skipPosition = getRandom(9, 1);
+  const quest = getDataWithSkipped(array, skipPosition);
+  const sol = String(array[skipPosition]);
   return cons(quest, sol);
 };
 
-export default () => makeGameFlow(gameLogic, rules);
+export default () => makeGameFlow(generateData, rules);
